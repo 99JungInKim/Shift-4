8f00341a03a2c078129e3cf1587f485afb880a83
JungIn <lego0520lwy@icloud.com>
2022-07-25 22:31:06 +0900

    작업중
    ㅁㅇㄴㄹㅁㄴㄹ

--- shift4/src/components/ServicesPage.vue
+++ shift4/src/components/ServicesPage.vue
@@ -1,19 +1,76 @@
 <template>
-  <div>
-    Services
+  <div class="Background">
+    <div class="Carousel">
+        <carousel-3d :controls-visible="true" :clickable="false">
+          <slide v-for="(slide, i) in slides" :index="i" :key="i">
+            <figure>
+              <figcaption>
+                {{i}}
+              </figcaption>
+              <img src="https://placehold.it/360x270">
+              <figcaption>
+                The sky is the limit only for those who aren't afraid to fly!
+                          The sky is the limit only for those who aren't afraid to fly!
+
+              </figcaption>
+            </figure>
+          </slide>
+      </carousel-3d>
+    </div>
   </div>
 </template>
 
 <script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+
 export default {
   name: 'ServicesPage',
+  data(){
+    return{
+        slides:[0,1,2,3,4]
+    }
+  },
   props: {
+  },
+  components:{
+    Carousel3d,
+    Slide
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.Background{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.Carousel{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.carousel-3d-container figure {
+  margin:0;
+}
+
+.carousel-3d-container figcaption{
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  top: 0;
+  position: absolute;
+  top: 0;
+  padding: 15px;
+  font-size: 12px;
+  min-width: 100%;
+  box-sizing: border-box;
+}
 </style>
 <style>
 </style>
--- shift4/src/main.js
+++ shift4/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Carousel3d from 'vue-carousel-3d'
 Vue.config.productionTip = false
-
+Vue.use(Carousel3d)
 new Vue({
   render: h => h(App),
   router
