d976aa98030dacea7acc64427b0c322b8acb6911
net6825 <80778023+net6825@users.noreply.github.com>
2022-07-25 22:31:08 +0900

    temp

--- shift4/src/components/LogInPage.vue
+++ shift4/src/components/LogInPage.vue
@@ -1,151 +1,193 @@
 <template>
   <div class="Background">
     <div class="Modal">
-        <div class="Left">
-            <img src="@/assets/icons/Shift4W.svg" @click="goHome()"/>
-            <span @click="isLogInMod=!isLogInMod">Go To {{(isLogInMod)?msg[1]:msg[0]}}</span>
+      <div class="Left">
+        <img src="@/assets/icons/Shift4W.svg" @click="goHome()"/>
+        <span @click="isLogInMod=!isLogInMod">Go To {{ (isLogInMod) ? msg[1] : msg[0] }}</span>
+      </div>
+      <div class="Right">
+        <span>{{ (isLogInMod) ? msg[0] : msg[1] }}</span>
+        <div class="inputArea">
+          <input type="text" placeholder="ID" v-model="id">
+          <input type="password" placeholder="PW" v-model="pw">
+          <input v-if="!isLogInMod" type="text" placeholder="NAME" v-model="name">
+          <input v-if="!isLogInMod" type="number" placeholder="PHONE" v-model="phone">
         </div>
-        <div class="Right">
-            <span>{{(isLogInMod)?msg[0]:msg[1]}}</span>
-            <div class="inputArea">
-                <input type="text" placeholder="ID" v-model="id">
-                <input type="password" placeholder="PW" v-model="pw">
-                <input v-if="!isLogInMod" type="text" placeholder="NAME" v-model="name">
-                <input v-if="!isLogInMod" type="number" placeholder="PHONE" v-model="phone">
-            </div>
-            <div class="LogInPageSubmit" @click="doWork(isLogInMod)">{{(isLogInMod)?msg[0]:msg[1]}}</div>
-        </div>
+        <div class="LogInPageSubmit" @click="doWork(isLogInMod)">{{ (isLogInMod) ? msg[0] : msg[1] }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+// import {doc, getDoc} from "firebase/firestore";
+
 export default {
   name: 'LogInPage',
-  props: {
-  },
-  data(){
-    return{
-        isLogInMod:true,
-        id:null,
-        pw:null,
-        name:null,
-        phone:null,
-        msg:["Log In","Sign Up"]
+  props: {},
+  data() {
+    return {
+      isLogInMod: true,
+      id: null,
+      pw: null,
+      name: null,
+      phone: null,
+      msg: ["Log In", "Sign Up"],
+      db: {}
     }
   },
-  methods : {
-    goHome(){
-        this.$router.push('/')
-    }, 
-    doWork(bool){
-        if(bool){//LogIn
-            alert("id : "+this.id+"\npw : "+this.pw)
+  methods: {
+    goHome() {
+      this.$router.push('/')
+    },
+    doWork(bool) {
+      const auth = getAuth();
+      if (bool) {//LogIn
+        alert("id : " + this.id + "\npw : " + this.pw)
+        signInWithEmailAndPassword(auth, this.id, this.pw)
+            .then((userCredential) => {
+              const loginUser = userCredential.user;
+              console.log("loginPage ", loginUser)
+              this.$store.state.User = loginUser;
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              console.log('errorCode ', errorCode, 'errorMessage ', errorMessage)
+            });
+      } else {//SignUp
+        alert("name : " + this.name + "\nphone : " + this.phone + "\nid : " + this.id + "\npw : " + this.pw)
+        if (this.pw.length < 6) {
+          alert('비밀번호는 6자리 이상 입력해야 합니다.')
+        } else {
+          createUserWithEmailAndPassword(auth, this.id, this.pw)
+              .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log('user ', user);
+                // insert DB
+                // 회원가입이 바로 되면 안되고 DB에 먼저 넣고 수락했을 때 회원가입이 되어야 하고, DB에 관리자 추가하고,
+                // 관리자는 관리자만 줄 수 있게
+              })
+              .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log('errorCode ', errorCode, 'errorMessage ', errorMessage)
+              });
         }
-        else{//SignUp
-            alert("name : "+this.name+"\nphone : "+this.phone+"\nid : "+this.id+"\npw : "+this.pw)
-        }
+      }
     }
   }
 }
 </script>
 <style scoped>
-.Background{
-    height: 100vh;
-    overflow: hidden;
-    margin:0;
-    padding: 0;
-    background-image: url('@/assets/backgrounds/wbg.jpeg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+.Background {
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  background-image: url('@/assets/backgrounds/wbg.jpeg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
-.Modal{
-    display: flex;
-    width: 800px;
-    height: 500px;
-    background-color: red;
-    border-radius: 20px;
-    filter: 
-    drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.5));
-}
-.Left{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 50%;
-    height: 100%;
-    border-radius: 20px 0 0 20px;
-    background-color: var(--e);
-}
-.Left img{
-    width: 70%;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-}
-.Left span{
-    color : var(--w);
-    font-weight: 100;
-    font-size: 30px;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-}
-.Right{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 50%;
-    height: 100%;
-    border-radius: 0 20px 20px 0;
-    background-color: var(--w);
-}
-.Right span{
-    color : var(--e);
-    font-size: 60px;
-    font-weight: 100;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-}
-.inputArea{
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 5%;
-    margin-bottom: 5%;
-}
-.inputArea input{
-    background: #E5E8EB;
-    border: none;
-    width: 70%;
-    height: 40px;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;
-    margin-bottom: 15px;
-    font-size: 20px;
-    font-weight: 100;
-    padding-left: 15px;
-}
-.LogInPageSubmit{
-    display: flex;
-    width: 100px;
-    height: 40px;
 
-    justify-content: center;
-    align-items: center;
-    color: var(--g1);
-    background: var(--g5);
-    border-radius: 10px;
+.Modal {
+  display: flex;
+  width: 800px;
+  height: 500px;
+  background-color: red;
+  border-radius: 20px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.5));
+}
 
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+.Left {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50%;
+  height: 100%;
+  border-radius: 20px 0 0 20px;
+  background-color: var(--e);
 }
-.LogInPageSubmit:hover{
-    background: var(--e);
+
+.Left img {
+  width: 70%;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 }
+
+.Left span {
+  color: var(--w);
+  font-weight: 100;
+  font-size: 30px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+.Right {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50%;
+  height: 100%;
+  border-radius: 0 20px 20px 0;
+  background-color: var(--w);
+}
+
+.Right span {
+  color: var(--e);
+  font-size: 60px;
+  font-weight: 100;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+.inputArea {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+
+.inputArea input {
+  background: #E5E8EB;
+  border: none;
+  width: 70%;
+  height: 40px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  margin-bottom: 15px;
+  font-size: 20px;
+  font-weight: 100;
+  padding-left: 15px;
+}
+
+.LogInPageSubmit {
+  display: flex;
+  width: 100px;
+  height: 40px;
+
+  justify-content: center;
+  align-items: center;
+  color: var(--g1);
+  background: var(--g5);
+  border-radius: 10px;
+
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+.LogInPageSubmit:hover {
+  background: var(--e);
+}
+
 input[type="number"]::-webkit-outer-spin-button,
 input[type="number"]::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
+  -webkit-appearance: none;
+  margin: 0;
 }
 </style>
--- shift4/src/main.js
+++ shift4/src/main.js
@@ -1,9 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { initializeApp } from "firebase/app"
+import { store } from "./store/store"
+
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6ZntyHJJ6DF2qIS2MqBFG_wr5sQmtOOU",
+  authDomain: "shift4-4009d.firebaseapp.com",
+  databaseURL: "https://shift4-4009d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shift4-4009d",
+  storageBucket: "shift4-4009d.appspot.com",
+  messagingSenderId: "165925142298",
+  appId: "1:165925142298:web:c340d092f8c28f02b176a3",
+  measurementId: "G-HD9BTPKT8R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app)
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store:store
 }).$mount('#app')
