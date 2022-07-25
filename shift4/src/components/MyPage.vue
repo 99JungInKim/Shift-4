923ac0987300076aa41d8a7166edd665a21546d9
JungIn <lego0520lwy@icloud.com>
2022-07-25 15:37:35 +0900

    MyPage Init

--- shift4/src/components/MyPage.vue
+++ shift4/src/components/MyPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    Mypage
+  <div class="Background">
+    <div class="Profile">
+        <div class="Circle" style="background: #BDBDBD;">
+            <img class="ProfileImg" src="@/assets/icons/Logo_W.svg">
+        </div>
+        <div class="Text">
+            <p>Name</p>
+            <p>Duty</p>
+            <p>Stack</p>
+        </div>
+    </div>
+    <div class="Card">
+        <div>
+
+        </div>
+    </div>
   </div>
 </template>
 
@@ -14,6 +28,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-</style>
-<style>
+.Background{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(var(--g5) 50%, var(--g1) 0%);
+}
+.Circle{
+    width: 150px;
+    height: 150px; 
+    border-radius: 50%;
+    overflow: hidden;
+}
+.ProfileImg{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
 </style>
