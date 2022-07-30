a362be7aba9976eb7af9821fdcbd4e62f58831bf
JungIn <lego0520lwy@icloud.com>
2022-07-30 18:35:30 +0900

    delete

--- shift4/src/components/ServicesPage.vue
+++ shift4/src/components/ServicesPage.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="Background">
-    <Agile :options="myOptions">
-      <div v-for="i in 10" :key="i"><img src="@/assets/backgrounds/wbg.jpeg"/></div>
 
-    </Agile>
+  <div class="Background">
   </div>
 </template>
+<script src="https://unpkg.com/vue-agile"></script>
+
 <script>
-import { VueAgile } from 'vue-agile';
 export default {
   name: "ServicesPage",
   data() {
@@ -37,7 +35,6 @@ export default {
   },
   props: {},
   components: {
-    Agile:VueAgile,
   },
   methods: {
     openService(i) {
@@ -91,9 +88,5 @@ export default {
   font-weight: 100;
   font-size: 2vw;
 }
-.agile{position:relative; width:100vw; height: 100vh;}
-.agile--ssr .agile__slides--cloned{display:none}
-.agile--ssr .agile__slides>*{overflow:hidden;width:0}
-.agile--ssr .agile__slides>:first-child{width:100%}.agile--rtl .agile__actions,.agile--rtl .agile__dots,.agile--rtl .agile__slides,.agile--rtl .agile__track{flex-direction:row-reverse}.agile:active,.agile :active,.agile:focus,.agile :focus{outline:none}.agile__list{display:block;overflow:hidden;position:relative;width:100%}.agile__track{display:flex;flex-direction:row;flex-wrap:nowrap}.agile__actions{display:flex;justify-content:space-between}.agile--no-nav-buttons .agile__actions{justify-content:center}.agile__slides{align-items:center;display:flex;flex-direction:row;flex-grow:1;flex-shrink:unset;flex-wrap:nowrap;justify-content:flex-start}.agile--disabled .agile__slides{display:block;width:100%}.agile__slide{display:block;flex-grow:1;flex-shrink:0}.agile__slide,.agile__slide *{-webkit-user-drag:none}.agile--fade .agile__slide{opacity:0;position:relative;z-index:0}.agile--fade .agile__slide--active{opacity:1;z-index:2}.agile--fade .agile__slide--expiring{opacity:1;transition-duration:0s;z-index:1}.agile__nav-button[disabled]{cursor:default}.agile__dots{align-items:center;display:flex;list-style:none;padding:0;white-space:nowrap}.agile__dot button{cursor:pointer;display:block;font-size:0;line-height:0}
 </style>
 <style></style>
--- shift4/src/main.js
+++ shift4/src/main.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueAgile from 'vue-agile'
 Vue.config.productionTip = false;
-Vue.use(VueAgile)
 new Vue({
   render: (h) => h(App),
   router,
