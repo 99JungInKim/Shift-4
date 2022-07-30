77388bb4d017afba449d676de61cdbcadb837e1a
starbong <bs9934@g.yju.ac.kr>
2022-07-30 16:55:04 +0900

    0730 bongsik

--- shift4/src/components/AboutUsPage.vue
+++ shift4/src/components/AboutUsPage.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="Background">
-    <div class="AboutUsLogo">TEST</div>
+    <div class="AboutUsLogo">
+      <img src="@/assets/icons/logoText.svg" />
+    </div>
     <div class="Information">
       <div class="LeftInfo"><img src="@/assets/backgrounds/bbg.jpeg" /></div>
       <div class="RightInfo"></div>
@@ -13,19 +15,54 @@
       <div class="LeftInfo"><img src="@/assets/backgrounds/bbg.jpeg" /></div>
       <div class="RightInfo"></div>
     </div>
-    <div class="AboutUsCarousel"></div>
-    <div class="AboutUsFooter">footer</div>
+    <p>우리의 포트폴리오를 소개합니다.</p>
+
+    <div class="AboutUsCarousel">
+      <carousel-3d
+        :disable3d="true"
+        :space="365"
+        :clickable="false"
+        :controls-visible="true"
+      >
+        <slide v-for="(slide, i) in slides" :index="i" :key="i">
+          <span class="title">포트폴리오 제목</span>
+          <p>
+            You know, being a test pilot isn't always the healthiest business in
+            the world.
+          </p>
+        </slide>
+      </carousel-3d>
+    </div>
+    <div class="AboutUsFooter">
+      <div>
+        <div class="SnsFont">sns</div>
+        <img src="@/assets/icons/Shift4W.svg" />
+      </div>
+
+      <div class="FooterFont">
+        <p class="tell">tel:010-6586-6842</p>
+        <p>대구광역시 북구 복현로 35</p>
+        <p>shift4 @ gmail.com</p>
+        <p>shift 4 채용문의</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Carousel3d, Slide } from "vue-carousel-3d";
+import "animate.css";
+
 export default {
   name: "AboutUsPage",
-  components: {},
+  components: {
+    Carousel3d,
+    Slide,
+  },
   props: {},
   data() {
     return {
-      slides: 6,
+      slides: [0, 1, 2, 3, 4],
     };
   },
 };
@@ -38,16 +75,62 @@ export default {
   justify-content: center;
   align-items: center;
 }
+
 .AboutUsLogo {
   width: 100vw;
   height: 100vh;
-  background-color: var(--b);
+  background-color: var(--g1);
+  display: inline-block;
+  padding: 10px;
+  animation: fadeIn 3s;
+  -moz-animation: fadeIn 3s; /* Firefox */
+  -webkit-animation: fadeIn 3s; /* Safari
+and Chrome */
+  -o-animation: fadeIn 3s; /* Opera */
 }
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@-moz-keyframes fadeIn {
+  /* Firefox */
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@-webkit-keyframes fadeIn {
+  /* Safari and
+Chrome */
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@-o-keyframes fadeIn {
+  /*
+Opera */
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
 .Information {
   width: 100vw;
   height: 50vw;
   font-size: 3vw;
-  background-color: var(--e);
+  background-color: var(--g1);
   border: 1px solid var(--b);
   display: flex;
   flex-direction: row;
@@ -75,22 +158,55 @@ export default {
 }
 .AboutUsCarousel {
   width: 100vw;
-  height: 30vw;
+  height: 70vh;
   display: flex;
-  background-color: red;
+  background-color: var(--g1);
   align-items: center;
   justify-content: center;
 }
-.AboutUsCarousel-3d-container {
+
+.carousel-3d-container {
   width: 100vw;
-  height: 30vw;
+  height: 100vh;
 }
-.carousel-3d-container figure {
-  margin: 0;
+.carousel-3d-slide {
+  border-radius: 20px 20px 20px 20px;
+  border: 40px;
+  padding: 20px;
 }
+.carousel-3d-title {
+  font-size: 22px;
+}
+
+.AboutUsTitle {
+  font-weight: 100;
+  font-size: 2vw;
+}
+
 .AboutUsFooter {
   width: 90vw;
-  height: 40vh;
+  height: 30vh;
   font-size: 3vw;
+}
+.AboutUsFooter img {
+  width: 10%;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 1));
+}
+
+.FooterFont {
+  font-size: 1.1vw;
+  position: relative;
+  left: 30vw;
+  font-weight: initial;
+  letter-spacing: 3px;
+  line-height: 200%;
+}
+.tell {
+  font-size: 1.5vw;
+}
+.SnsFont {
+  position: relative;
+  left: 70vw;
+  top: 8vw;
 }
 </style>
